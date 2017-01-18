86792e2acc15cf7c6b7369d41f8f818ba5d29414
Alison Winters <alisonatwork@gmail.com>
2017-01-18 13:06:05 +0100

    test(*) use new .get() methods instead of [] object accessor

--- spec/filters/google_appengine_spec.rb
+++ spec/filters/google_appengine_spec.rb
@@ -19,20 +19,20 @@ describe LogStash::Filters::GoogleAppengine do
       LATENCY_OF_REQUEST = 0.115752
       insist { subject.length } == 3
 
-      insist { subject[0]["message"] }=="IdentityFilter logUserIdentity: [[meta]] <anonymous:true>\n"
-      insist { subject[0]["id"] }== md5.hexdigest(subject[0]["requestId"] + "0")
-      insist { subject[0]["time"] } == "2015-09-03T10:59:40.589Z"
-      insist { subject[0]["position"] } == 0
-      insist { subject[0]["@type"] } == nil
-      insist { subject[0]["latencyS"] } == LATENCY_OF_REQUEST
-      insist { subject[0]["pendingTimeS"] } == nil
+      insist { subject[0].get("message") }=="IdentityFilter logUserIdentity: [[meta]] <anonymous:true>\n"
+      insist { subject[0].get("id") }== md5.hexdigest(subject[0].get("requestId") + "0")
+      insist { subject[0].get("time") } == "2015-09-03T10:59:40.589Z"
+      insist { subject[0].get("position") } == 0
+      insist { subject[0].get("@type") } == nil
+      insist { subject[0].get("latencyS") } == LATENCY_OF_REQUEST
+      insist { subject[0].get("pendingTimeS") } == nil
 
-      insist { subject[1]["message"] }=="HttpOnlyFilter getSession: add additional Set-Cookie with httpOnly-flag for JSESSIONID\n"
-      insist { subject[1]["id"] } == md5.hexdigest(subject[1]["requestId"] + "1")
-      insist { subject[1]["@type"] } == nil
-      insist { subject[1]["time"] } =="2015-09-03T10:59:40.65Z"
-      insist { subject[1]["position"] } == 1
-      insist { subject[0]["pendingTimeS"] } == nil
+      insist { subject[1].get("message") }=="HttpOnlyFilter getSession: add additional Set-Cookie with httpOnly-flag for JSESSIONID\n"
+      insist { subject[1].get("id") } == md5.hexdigest(subject[1].get("requestId") + "1")
+      insist { subject[1].get("@type") } == nil
+      insist { subject[1].get("time") } =="2015-09-03T10:59:40.65Z"
+      insist { subject[1].get("position") } == 1
+      insist { subject[0].get("pendingTimeS") } == nil
     end
   end
 
@@ -40,10 +40,10 @@ describe LogStash::Filters::GoogleAppengine do
     test_sample = LogStash::Json.load(File.open("spec/filters/appengine.logs-without-lines.jsonl", "rb").read)
     sample (test_sample) do
 
-      insist { subject["resource"] } == "/images/website/welcome/keyFeatures/objectives.jpg"
-      insist { subject["id"] } == md5.hexdigest(subject["requestId"])
-      insist { subject["time"] } == subject["endTime"]
-      insist { subject["@type"] } == nil
+      insist { subject.get("resource") } == "/images/website/welcome/keyFeatures/objectives.jpg"
+      insist { subject.get("id") } == md5.hexdigest(subject.get("requestId"))
+      insist { subject.get("time") } == subject.get("endTime")
+      insist { subject.get("@type") } == nil
     end
   end
 end
